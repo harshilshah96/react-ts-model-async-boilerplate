29da8669d6770dced8966821f81ac56b4da5cf18
Harshil <harshil.shah@coditas.com>
2019-01-09 11:22:04 +0530

    Reset

--- ambientInterfaces.ts
+++
@@ -1,3 +0,0 @@
-declare interface Function {
-    resource?: string;
-}
--- interfaces.ts
+++
@@ -1,8 +0,0 @@
-import * as Immutable from 'immutable';
-import { BaseModel } from './src/Models/BaseModel';
-import { IAsyncState } from './src/components/ReusableComponents/Async';
-
-export interface IState {
-    models: Immutable.Map<string, BaseModel<{}>>;
-    loading: Immutable.Map<string, IAsyncState>;
-}
